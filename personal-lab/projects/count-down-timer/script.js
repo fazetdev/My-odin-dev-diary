a3f90385f42f9c52ebc8f8b6e766e005566f4ae1
fazetdev <fazetdev@gmail.com>
2025-07-26 18:57:02 +0100

    Update countdown timer and related files

---
+++ personal-lab/projects/count-down-timer/script.js
@@ -0,0 +1,21 @@
+const startBtn = document.querySelector("#start-btn");
+
+startBtn.addEventListener("click", () => {
+  const input = document.querySelector("#datetime");
+  const targetDate = new Date(input.value);
+
+  setInterval(() => {
+    const now = new Date();
+    const timeRemaining = targetDate - now;
+
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemaining / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((timeRemaining / (1000 * 60)) % 60);
+    const seconds = Math.floor((timeRemaining / 1000) % 60);
+
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+  }, 1000);
+});
